dd79f8b91b97ff99629c1b9bbbbab54f7c3bf60d
mattpassos <70536543+mattpassos@users.noreply.github.com>
2021-05-26 23:04:43 +0200

    Updated with 4 Pages on the Menu below

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import { ellipse, square, triangle } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 import Tab3 from './pages/Tab3';
+import Tab4 from './pages/Tab4';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -46,6 +47,9 @@ const App: React.FC = () => (
           </Route>
           <Route path="/tab3">
             <Tab3 />
+            </Route>
+          <Route path="/tab4">
+            <Tab4 />
           </Route>
           <Route exact path="/">
             <Redirect to="/tab1" />
@@ -53,16 +57,20 @@ const App: React.FC = () => (
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
+            <IonIcon icon={square} />
             <IonLabel>Tab 1</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
+            <IonIcon icon={square} />
             <IonLabel>Tab 2</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
             <IonIcon icon={square} />
             <IonLabel>Tab 3</IonLabel>
+            </IonTabButton>
+          <IonTabButton tab="tab4" href="/tab4">
+            <IonIcon icon={square} />
+            <IonLabel>Tab 4</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
