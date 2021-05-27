d45f6c04a4802a0b01193d26bbe538e2652d53c0
loraxxde <llaura.dobson@gmail.com>
2021-05-28 01:38:46 +0200

    Cleaned up App,tsx by adding all of the menu functionality into a component called BottomTab

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,7 +8,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Home</IonTitle>
+          <IonTitle>What Street Art</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
