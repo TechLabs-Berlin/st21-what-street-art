50b303df2133458080a47ec35d9e5c94d83d4590
loraxxde <llaura.dobson@gmail.com>
2021-06-02 19:48:55 +0200

    Removed header from page

--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -4,22 +4,13 @@ import './Explore.css';
 
 const Explore: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Explore</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Explore</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Explore" />
-      </IonContent>
-    </IonPage>
-  );
-};
+        <IonPage>
+          <IonContent fullscreen>
+            <ExploreContainer name="Explore" />
+          </IonContent>
+        </IonPage>
+      );
+    };
+    
 
 export default Explore;
