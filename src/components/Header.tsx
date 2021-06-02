8554798e77b08fc40ce927febb839d86dbcfd2a1
loraxxde <llaura.dobson@gmail.com>
2021-06-02 21:10:31 +0200

    Added 'Settings' tab icon that routes to 'Settings' page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,45 @@
 import React from 'react';
-import { IonHeader, IonToolbar, IonTitle } from '@ionic/react';
+import { Redirect, Route } from 'react-router-dom';
+import { IonHeader, IonToolbar, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonTitle, IonRouterOutlet } from '@ionic/react';
+import { settingsOutline } from 'ionicons/icons';
 import './Header.css';
+import Settings from '../pages/Settings';
+import { IonReactRouter } from '@ionic/react-router';
+
 
 
 const Header: React.FC = () => {
 return (
-    <IonHeader>
-      <IonToolbar>
-        <IonTitle class="headerTitle">What Street Art</IonTitle>
-      </IonToolbar>
-    </IonHeader>
-);
+    <IonToolbar>
+        <h1 className="headerTitle">What Street Art</h1>
+        <IonReactRouter>
+            
+    <IonTabs>
+        <IonRouterOutlet>
+
+{/* Routes */}
+    {/* Settings */}
+            <Route exact path="/Settings">
+                <Settings />
+            </Route>
+        </IonRouterOutlet>
+
+
+<IonTabBar slot="top">
+
+
+{/* Tabs */}
+  {/* Settings */}
+  <IonTabButton tab="Settings" href="/Settings">
+           <IonIcon icon={settingsOutline} />
+          </IonTabButton>
+          <IonTitle>What Street Art</IonTitle>
+          </IonTabBar>
+      </IonTabs>
+    </IonReactRouter>
+    </IonToolbar>
+  );
 };
 
 export default Header;
+
