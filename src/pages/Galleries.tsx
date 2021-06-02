bdbd9249d1c6d231a4d519c60e9b4eccfe20d240
loraxxde <llaura.dobson@gmail.com>
2021-06-02 19:50:27 +0200

    Removed header from page

--- src/pages/Galleries.tsx
+++ src/pages/Galleries.tsx
@@ -4,22 +4,12 @@ import './Galleries.css';
 
 const Galleries: React.FC = () => {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Galleries</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Galleries</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Galleries" />
-      </IonContent>
-    </IonPage>
-  );
+  <IonPage>
+    <IonContent fullscreen>
+      <ExploreContainer name="Galleries" />
+    </IonContent>
+  </IonPage>
+);
 };
 
 export default Galleries;
