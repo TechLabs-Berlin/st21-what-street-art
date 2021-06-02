9c19446587c8dd8fd0871a6f710b8f380fc5dbf9
loraxxde <llaura.dobson@gmail.com>
2021-06-02 22:46:59 +0200

    Added Settings button to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,45 +1,25 @@
 import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
-import { IonHeader, IonToolbar, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel, IonTitle, IonRouterOutlet } from '@ionic/react';
+import { IonHeader, IonToolbar, IonButtons, IonButton, IonIcon, IonTitle } from '@ionic/react';
 import { settingsOutline } from 'ionicons/icons';
 import './Header.css';
-import Settings from '../pages/Settings';
-import { IonReactRouter } from '@ionic/react-router';
 
 
 
 const Header: React.FC = () => {
 return (
-    <IonToolbar>
-        <h1 className="headerTitle">What Street Art</h1>
-        <IonReactRouter>
-            
-    <IonTabs>
-        <IonRouterOutlet>
-
-{/* Routes */}
-    {/* Settings */}
-            <Route exact path="/Settings">
-                <Settings />
-            </Route>
-        </IonRouterOutlet>
-
-
-<IonTabBar slot="top">
-
-
-{/* Tabs */}
-  {/* Settings */}
-  <IonTabButton tab="Settings" href="/Settings">
-           <IonIcon icon={settingsOutline} />
-          </IonTabButton>
-          <IonTitle>What Street Art</IonTitle>
-          </IonTabBar>
-      </IonTabs>
-    </IonReactRouter>
-    </IonToolbar>
-  );
+    <IonHeader>
+        <IonToolbar>
+            <IonTitle className="headerTitle">What Street Art</IonTitle>
+            <IonButtons slot="primary">
+            <IonButton href="/Settings">
+          <IonIcon icon={settingsOutline}/>
+        </IonButton>
+            </IonButtons>
+        </IonToolbar>
+    </IonHeader>
+);
 };
+
 
 export default Header;
 
