91c7841eb2b3a0e91031cd9d8bce5554413df28f
loraxxde <llaura.dobson@gmail.com>
2021-06-02 19:54:01 +0200

    Replaced Near.tsx with Map.tsx

---
+++ src/pages/Near.tsx
@@ -0,0 +1,28 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import ExploreContainer from '../components/ExploreContainer';
+import './Near.css';
+
+const Near: React.FC = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Near You</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Near You</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer name="Near You" />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Near;
+
+// See below website:
+// https://www.techiediaries.com/ionic-geolocation-google-maps-places-api/
