2dc6f8e9f596b3ef49c837ce5fbc2acabe79405f
loraxxde <llaura.dobson@gmail.com>
2021-06-02 19:51:56 +0200

    Created Header component that is called in App.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { IonHeader, IonToolbar, IonTitle } from '@ionic/react';
+
+
+const Header: React.FC = () => {
+return (
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>What Street Art</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+);
+};
+
+export default Header;
