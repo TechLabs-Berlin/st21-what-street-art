694f683ea4f6785c28533fb955a116d7a48e0c19
mattpassos <70536543+mattpassos@users.noreply.github.com>
2021-06-04 23:17:09 +0200

    Added SwiperSlide @Galleries page w/ sample images

--- src/pages/Galleries.tsx
+++ src/pages/Galleries.tsx
@@ -1,60 +1,67 @@
-import { IonContent, IonPage, IonSlides, IonCard, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/react';
+import React, { useRef, useState } from "react";
+import { IonContent, IonPage, IonSlides, IonSlide, IonText } from '@ionic/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 import ExploreContainer from '../components/ExploreContainer';
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.min.css"
 import Header from '../components/Header';
 import './Galleries.css';
+import SwiperCore, {
+  Pagination
+} from 'swiper/core';
 
+// install Swiper modules
+SwiperCore.use([Pagination]);
 
-const Galleries: React.FC = () => {
+export const Galleries: React.FC = () => {
+  const mySlides = useRef(null);
+
   return (
   <IonPage>
     <Header />
-    <IonContent>
-    <IonSlides>
-<IonCard>
-  <IonCardContent>
-    <IonCardHeader>
-    <IonCardTitle>Popular</IonCardTitle>
-    </IonCardHeader>
-    <img src="https://www.tip-berlin.de/wp-content/uploads/2020/05/imago0079536013h-1600x1068.jpg" alt="Angriff der 20-Meter-Frau"/>
-    <IonCardTitle>
-      Angriff der 20-Meter-Frau, Tristan Eaton
-      </IonCardTitle>
-  </IonCardContent>
-</IonCard>
-</IonSlides>
-
-<IonSlides>
-<IonCard>
-  <IonCardContent>
-  <IonCardHeader>
-    <IonCardTitle>Recently Added</IonCardTitle>
-    </IonCardHeader>
-    <img src="https://www.tip-berlin.de/wp-content/uploads/2020/05/imago0081188948h-1536x1024.jpg" alt="Angriff der 20-Meter-Frau"/>
-    <IonCardTitle>
-      Test the Best, Birgit Kinder
-      </IonCardTitle>
-  </IonCardContent>
-</IonCard>
-</IonSlides>
-
-<IonSlides>
-<IonCard>
-  <IonCardContent>
-  <IonCardHeader>
-    <IonCardTitle>Near Me</IonCardTitle>
-    </IonCardHeader>
-    <img src="https://www.tip-berlin.de/wp-content/uploads/2020/05/imago0081188948h-1536x1024.jpg" alt="Angriff der 20-Meter-Frau"/>
-    <IonCardTitle>
-      Test the Best, Birgit Kinder
-      </IonCardTitle>
-  </IonCardContent>
-</IonCard>
-</IonSlides>
-
-    
+      <IonContent>
+        <div>
+        <Swiper slidesPerView={2} spaceBetween={10} pagination={{"clickable": true}} className="mySwiper1">
+          <SwiperSlide> <img src="https://velvetescape.com/wp-content/uploads/2018/02/IMG_4081-1280x920.jpg" /></SwiperSlide>
+          <SwiperSlide> <img src="https://hierdadort.de/wp-content/uploads/2020/05/P1000650-Cover_1800_Vogel.jpg" /></SwiperSlide>
+          <SwiperSlide> <img src="https://www.tip-berlin.de/wp-content/uploads/2020/05/imago0092310228h-800x517.jpg" /></SwiperSlide>
+          <SwiperSlide> <img src="https://awesomeberlin.net/wp-content/uploads/2015/01/IMG_1704.jpg" /></SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+        </Swiper>
+        </div>
+        <div>
+        <Swiper slidesPerView={2} spaceBetween={10} pagination={{"clickable": true}} className="mySwiper2">
+          <SwiperSlide> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Wandbild_Portrait_George_Floyd_von_Eme_Street_Art_im_Mauerpark_%28Berlin%29.jpg/2560px-Wandbild_Portrait_George_Floyd_von_Eme_Street_Art_im_Mauerpark_%28Berlin%29.jpg" /></SwiperSlide>
+          <SwiperSlide> <img src="https://assets.atlasobscura.com/media/W1siZiIsInVwbG9hZHMvcGxhY2VfaW1hZ2VzL2U4NjdhZjhpZm5icDd5YTUwN2NhM2JlNWUwYzQyZTBiOTVfMTEyMzExNjNfMTAxNTYwNDczNDkwOTA0MzRfNzgxMjkzNzE3NjA4ODIyMDAxMF9vLmpwZyJdLFsicCIsInRodW1iIiwiNTgweDU4MCMiXSxbInAiLCJjb252ZXJ0IiwiLXF1YWxpdHkgODEgLWF1dG8tb3JpZW50Il1d/11231163_10156047349090434_7812937176088220010_o.jpg" /></SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+        </Swiper>
+        </div>
+        <div>
+        <Swiper slidesPerView={2} spaceBetween={10} pagination={{"clickable": true}} className="mySwiper3">
+          <SwiperSlide>Near You</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+        </Swiper>
+        </div>
     </IonContent>
   </IonPage>
-);
+  );
 };
 
 export default Galleries;
