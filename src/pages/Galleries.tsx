74bb96bb638f7a9c7064f299e0a4bd6f61f4003f
loraxxde <llaura.dobson@gmail.com>
2021-06-05 23:56:05 +0200

    Added info-block to image sliders

--- src/pages/Galleries.tsx
+++ src/pages/Galleries.tsx
@@ -13,33 +13,83 @@ import SwiperCore, {
 // install Swiper modules
 SwiperCore.use([Pagination]);
 
+
+
+
 export const Galleries: React.FC = () => {
   const mySlides = useRef(null);
-
   return (
   <IonPage>
     <Header />
       <IonContent>
 
-        <h4>Popular</h4>
-        <div>
+      <h4>Popular</h4>
+      <div>
         <Swiper slidesPerView={2} spaceBetween={-10} loop={true} className="mySwiper1">
-          <SwiperSlide> <img src="https://velvetescape.com/wp-content/uploads/2018/02/IMG_4081-1280x920.jpg" /></SwiperSlide>
-          <SwiperSlide> <img src="https://hierdadort.de/wp-content/uploads/2020/05/P1000650-Cover_1800_Vogel.jpg" /></SwiperSlide>
-          <SwiperSlide> <img src="https://www.tip-berlin.de/wp-content/uploads/2020/05/imago0092310228h-800x517.jpg" /></SwiperSlide>
-          <SwiperSlide> <img src="https://awesomeberlin.net/wp-content/uploads/2015/01/IMG_1704.jpg" /></SwiperSlide>
-          <SwiperSlide>Slide 5</SwiperSlide>
-          <SwiperSlide>Slide 6</SwiperSlide>
-          <SwiperSlide>Slide 7</SwiperSlide>
-          <SwiperSlide>Slide 8</SwiperSlide>
-          <SwiperSlide>Slide 9</SwiperSlide>
-        </Swiper>
+          <SwiperSlide> 
+            <img src="https://velvetescape.com/wp-content/uploads/2018/02/IMG_4081-1280x920.jpg" />
+            <div className="info-block">
+              <h5>Title/Artist</h5>
+            </div>
+            </SwiperSlide>
+
+          <SwiperSlide> 
+            <img src="https://hierdadort.de/wp-content/uploads/2020/05/P1000650-Cover_1800_Vogel.jpg" />
+            <div className="info-block">
+              <h5>Title/Artist</h5>
+            </div>
+          </SwiperSlide>
+          
+          <SwiperSlide> 
+            <img src="https://www.tip-berlin.de/wp-content/uploads/2020/05/imago0092310228h-800x517.jpg" />
+            <div className="info-block">
+              <h5>Title/Artist</h5>
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide> 
+            <img src="https://awesomeberlin.net/wp-content/uploads/2015/01/IMG_1704.jpg" />
+            <div className="info-block">
+              <h5>Title/Artist</h5>
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide>Slide 5
+          <div className="info-block">
+            <h5>Title/Artist</h5>
+          </div>
+          </SwiperSlide>
+
+          <SwiperSlide>Slide 6
+          <div className="info-block">
+            <h5>Title/Artist</h5>
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide>Slide 7
+          <div className="info-block">
+            <h5>Title/Artist</h5>
+          </div>
+          </SwiperSlide>
+
+          <SwiperSlide>Slide 8
+          <div className="info-block">
+            <h5>Title/Artist</h5>
+          </div>
+          </SwiperSlide>
+
+          <SwiperSlide>Slide 9
+          <div className="info-block">
+            <h5>Title/Artist</h5>
+          </div>
+          </SwiperSlide>
+          </Swiper>
         </div>
 
         <h4>Recently Added</h4>
         <div>
         <Swiper slidesPerView={2} spaceBetween={-10} loop={true} className="mySwiper2">
-          <SwiperSlide> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Wandbild_Portrait_George_Floyd_von_Eme_Street_Art_im_Mauerpark_%28Berlin%29.jpg/2560px-Wandbild_Portrait_George_Floyd_von_Eme_Street_Art_im_Mauerpark_%28Berlin%29.jpg" /></SwiperSlide>
+          <SwiperSlide> <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Wandbild_Portrait_George_Floyd_von_Eme_Street_Art_im_Mauerpark_%28Berlin%29.jpg/2560px-Wandbild_Portrait_George_Floyd_von_Eme_Street_Art_im_Mauerpark_%28Berlin%29.jpg"/></SwiperSlide>
           <SwiperSlide> <img src="https://assets.atlasobscura.com/media/W1siZiIsInVwbG9hZHMvcGxhY2VfaW1hZ2VzL2U4NjdhZjhpZm5icDd5YTUwN2NhM2JlNWUwYzQyZTBiOTVfMTEyMzExNjNfMTAxNTYwNDczNDkwOTA0MzRfNzgxMjkzNzE3NjA4ODIyMDAxMF9vLmpwZyJdLFsicCIsInRodW1iIiwiNTgweDU4MCMiXSxbInAiLCJjb252ZXJ0IiwiLXF1YWxpdHkgODEgLWF1dG8tb3JpZW50Il1d/11231163_10156047349090434_7812937176088220010_o.jpg" /></SwiperSlide>
           <SwiperSlide>Slide 3</SwiperSlide>
           <SwiperSlide>Slide 4</SwiperSlide>
