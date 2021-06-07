332ad8f6fab40e29a897a2a899c66a957f7b9d9f
Daryna Kubar <darina.kubar@gmail.com>
2021-06-07 09:30:06 +0200

    Try to run near_you.py with Flask

--- DS/near_you.py
+++ DS/near_you.py
@@ -1,6 +1,7 @@
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
 import pandas as pd
+from flask import Flask
 
 df_final = pd.read_csv('data_final.csv')
 df_final['lat_lng'] = list(zip(df_final['location_lat'], df_final['location_lng']))
@@ -13,9 +14,22 @@ input_address_to_coord = geolocator.geocode(input_address)
 input_coord = (input_address_to_coord.latitude, input_address_to_coord.longitude)
 print(input_coord)
 
+def distance(value1, value2):
+    new_value = geodesic(value1, value2).km
+    return new_value
+
 for coord in location:
-    distance = geodesic(input_coord, coord).km
-    if distance <= 2:
+    dis = distance(input_coord, coord)
+    if dis <= 2:
         index = location[location == coord].index[0]
         result = df_final.id[index].tolist()
         print(result)
+        print(type(result))
+
+# app = Flask(__name__)
+# @app.route('/')
+# def index():
+#     return result
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
