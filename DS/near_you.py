d63b30ea1e7a4784538398bed35eff3643652383
Daryna Kubar <darina.kubar@gmail.com>
2021-06-07 15:43:40 +0200

    Output a dictionary of type {id:distance}

--- DS/near_you.py
+++ DS/near_you.py
@@ -12,24 +12,25 @@ geolocator = Nominatim(timeout=20, user_agent = 'what_street_art')
 input_address = '31, Tasdorfer Str, Berlin, 10365, Deutschland'
 input_address_to_coord = geolocator.geocode(input_address)
 input_coord = (input_address_to_coord.latitude, input_address_to_coord.longitude)
-print(input_coord)
 
 def distance(value1, value2):
     new_value = geodesic(value1, value2).km
     return new_value
 
+ids_dict = {}
+
 for coord in location:
-    dis = distance(input_coord, coord)
+    dis = round(distance(input_coord, coord),2)
     if dis <= 2:
         index = location[location == coord].index[0]
-        result = df_final.id[index].tolist()
-        print(result)
-        print(type(result))
+        ids_dict[index] = dis
+print(ids_dict)
 
-app = Flask(__name__)
-@app.route('/')
-def index():
-    return result
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# app = Flask(__name__)
+# @app.route('/')
+# def index():
+#     return ids_dict
+#
+# if __name__ == '__main__':
+#     app.run(debug=True)
