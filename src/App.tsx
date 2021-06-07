b1b848eb2d603a8dbe10926eb10b09ae8f485733
Matheus Albuquerque <land@ythecombinator.space>
2021-06-07 18:45:37 +0200

    feat(src/App.tsx): wrap App with firestore

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,38 @@
-import {
-  IonApp,
-} from '@ionic/react';
-import BottomMenu from './components/BottomMenu';
-import Header from './components/Header';
+import { IonApp } from "@ionic/react";
+import BottomMenu from "./components/BottomMenu";
+import Header from "./components/Header";
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
+import "@ionic/react/css/core.css";
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
 
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
+import { FirebaseAppProvider } from "reactfire";
+import { firebaseConfig } from "./config";
+import { Suspense } from "react";
 
 const App: React.FC = () => (
-  <IonApp>
-    <Header />
-    <BottomMenu />
-  </IonApp>
+  <Suspense fallback={<div>Test</div>}>
+    <IonApp>
+      <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+        <Header />
+        <BottomMenu />
+      </FirebaseAppProvider>
+    </IonApp>
+  </Suspense>
 );
 
 export default App;
