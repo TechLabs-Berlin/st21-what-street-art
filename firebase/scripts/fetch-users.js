87f896916df6d5ecf77c5c60e72eeed8b7e62f73
Emma <carballal.emma@gmail.com>
2021-06-11 11:36:07 +0200

    Convert mock user csv to json

---
+++ firebase/scripts/fetch-users.js
@@ -0,0 +1,33 @@
+const csv = require("csvtojson");
+const fs = require("fs-extra");
+const { logger } = require("./_shared");
+const path = require("path");
+const admin = require("firebase-admin");
+
+const config = {
+  input: "../../DS/users.csv",
+  output: `./config/collections/users.json`,
+};
+
+const fetchUsers = async () => {
+  const operation = `Converting .csv file from: ${config.input}`;
+  logger.init(operation);
+
+  const rawResult = await csv().fromFile(path.resolve(__dirname, config.input));
+
+  const result = rawResult.map((item) => ({
+    id: item.id,
+    email: item.email,
+    country: item.country,
+    liked: Array(item.liked),
+    saved: Array(item.saved)
+  }));
+
+  await fs.outputFile(config.output, JSON.stringify(result, null, 2));
+
+  logger.finish(operation);
+};
+
+fetchUsers();
+
+// console.log(path.resolve(__dirname, "../../DS/users.csv"));
