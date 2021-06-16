63e63992e268abde3559cf428916578acf023a4f
loraxxde <llaura.dobson@gmail.com>
2021-06-16 21:17:48 +0200

    Galleries code inputted into Home.tsx & .css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,76 +1,45 @@
-import { IonCard, IonCardTitle, IonCardContent, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCardHeader, IonSlides } from '@ionic/react';
-import { options } from 'ionicons/icons';
+import React, { useRef, useState } from "react";
+import {
+  IonContent,
+  IonPage
+} from "@ionic/react";
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.min.css";
+import Header from "../components/Header";
+import SwiperCore, { Pagination } from "swiper/core";
+import { usePopularArtworks } from "../hooks/artworks";
+import AssetSlider from "../components/AssetSlider";
+
 import './Home.css';
 
-const Home: React.FC = () => {
+
+// install Swiper modules
+SwiperCore.use([Pagination]);
+
+
+
+
+export const Home: React.FC = () => {
+  const popularArtworksData = usePopularArtworks({
+    limit: 9,
+    property: "likes",
+  });
+
+  const recentArtworksData = usePopularArtworks({
+    limit: 9,
+    property: "dateAdded",
+  });
+
   return (
-
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Home</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-
-{/* Popular Horizontal Slide */}
-{/* https://swiperjs.com/react */}
-
-<IonSlides>
-<IonCard>
-  <IonCardContent>
-    <IonCardHeader>
-    <IonCardTitle>Popular</IonCardTitle>
-    </IonCardHeader>
-    <img src="https://www.tip-berlin.de/wp-content/uploads/2020/05/imago0079536013h-1600x1068.jpg" alt="Angriff der 20-Meter-Frau"/>
-    <IonCardTitle>
-      Angriff der 20-Meter-Frau, Tristan Eaton
-      </IonCardTitle>
-  </IonCardContent>
-</IonCard>
-</IonSlides>
-
-<IonSlides>
-<IonCard>
-  <IonCardContent>
-    <img src="https://www.tip-berlin.de/wp-content/uploads/2020/05/imago0081188948h-1536x1024.jpg" alt="Angriff der 20-Meter-Frau"/>
-    <IonCardTitle>
-      Test the Best, Birgit Kinder
-      </IonCardTitle>
-  </IonCardContent>
-</IonCard>
-</IonSlides>
-
-
+      <Header />
+      <IonContent>
+        <AssetSlider title="Popular" data={popularArtworksData} />
+        <AssetSlider title="Recently Added" data={recentArtworksData} />
+        <AssetSlider title="Near you" data={popularArtworksData} />
       </IonContent>
     </IonPage>
   );
 };
 
 export default Home;
-
-// My attempt to write an menu without success:
-//     <IonPage>
-//     <IonMenu menuId="mainMenu" contentId="main">
-//       <IonHeader>
-//         <IonToolbar>
-//           <IonTitle>Home</IonTitle>
-//           <IonButtons slot="start">
-//             <IonMenuButton></IonMenuButton>
-//           </IonButtons>
-//         </IonToolbar>
-//       </IonHeader>
-//       <ExploreContainer name="Home" />
-//       <IonContent>
-//         {/* <IonList>
-//           <IonItem>
-//             <IonIcon icon={menu} slot="mail"></IonIcon>
-//             <IonLabel>Inbox</IonLabel>
-//           </IonItem>
-//         </IonList> */}
-//       </IonContent>
-//     </IonMenu>
-//   </IonPage>
-//   );
-// };
-
