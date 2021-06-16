a92c89269967fc194281290b29fbd5e2903b4052
mattpassos <70536543+mattpassos@users.noreply.github.com>
2021-06-16 23:00:05 +0200

    Revert "Cleaned Home page code"
    This reverts commit 02f5fd64ff5e83d433a7062bd87f994e49414338.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCard, IonCardTitle, IonCardContent, IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonCardHeader, IonSlides } from '@ionic/react';
+import { options } from 'ionicons/icons';
 import './Home.css';
 
 const Home: React.FC = () => {
@@ -12,9 +13,64 @@ const Home: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen>
 
+{/* Popular Horizontal Slide */}
+{/* https://swiperjs.com/react */}
+
+<IonSlides>
+<IonCard>
+  <IonCardContent>
+    <IonCardHeader>
+    <IonCardTitle>Popular</IonCardTitle>
+    </IonCardHeader>
+    <img src="https://www.tip-berlin.de/wp-content/uploads/2020/05/imago0079536013h-1600x1068.jpg" alt="Angriff der 20-Meter-Frau"/>
+    <IonCardTitle>
+      Angriff der 20-Meter-Frau, Tristan Eaton
+      </IonCardTitle>
+  </IonCardContent>
+</IonCard>
+</IonSlides>
+
+<IonSlides>
+<IonCard>
+  <IonCardContent>
+    <img src="https://www.tip-berlin.de/wp-content/uploads/2020/05/imago0081188948h-1536x1024.jpg" alt="Angriff der 20-Meter-Frau"/>
+    <IonCardTitle>
+      Test the Best, Birgit Kinder
+      </IonCardTitle>
+  </IonCardContent>
+</IonCard>
+</IonSlides>
+
+
       </IonContent>
     </IonPage>
   );
 };
 
 export default Home;
+
+// My attempt to write an menu without success:
+//     <IonPage>
+//     <IonMenu menuId="mainMenu" contentId="main">
+//       <IonHeader>
+//         <IonToolbar>
+//           <IonTitle>Home</IonTitle>
+//           <IonButtons slot="start">
+//             <IonMenuButton></IonMenuButton>
+//           </IonButtons>
+//         </IonToolbar>
+//       </IonHeader>
+//       <ExploreContainer name="Home" />
+//       <IonContent>
+//         {/* <IonList>
+//           <IonItem>
+//             <IonIcon icon={menu} slot="mail"></IonIcon>
+//             <IonLabel>Inbox</IonLabel>
+//           </IonItem>
+//         </IonList> */}
+//       </IonContent>
+//     </IonMenu>
+//   </IonPage>
+//   );
+// };
+
