0b2a93ca0d58fb8cc646cf5145587b6887273d8e
loraxxde <llaura.dobson@gmail.com>
2021-06-25 01:42:40 +0200

    Added Markers populated w/ JSON data

--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -2,17 +2,27 @@ import { FC } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "./LeafletMap.css";
 
+import ArtSites from "../data/artworkLocations.json";
+
 const position = { lat: 52.5200, lng: 13.4050 };
 
 const LeafletMap: FC = () => {
+
   return (
     <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
+      />
+      
+      
+      {ArtSites.map(artw => (
+      <Marker 
+      key = {artw.id}
+      position = {[artw["location/lat"], artw["location/lng"]]}>
       </Marker>
+
+    ))}
+  
+
       <div id="leaflet-map"></div>
     </MapContainer>
   );
