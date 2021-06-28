e43ea9fa2d18f15ed27d0b30c661df8a21a570ec
mattpassos <70536543+mattpassos@users.noreply.github.com>
2021-06-28 19:17:18 +0200

    Changed Fab button to circle

--- src/pages/Scan.tsx
+++ src/pages/Scan.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { IonContent, IonPage, IonTitle, IonToolbar, IonFab, IonFabButton, IonIcon, IonGrid, IonRow, IonCol, IonImg, IonActionSheet } from '@ionic/react';
-import { cameraOutline, trash, close } from 'ionicons/icons'
+import { trash, close, radioButtonOffOutline } from 'ionicons/icons'
 import ExploreContainer from '../components/ExploreContainer';
 import Header from '../components/Header';
 import './Scan.css';
@@ -26,7 +26,7 @@ const Scan: React.FC = () => {
         </IonGrid>
         <IonFab vertical="bottom" horizontal="center" slot="fixed">
           <IonFabButton onClick={() => takePhoto()}>
-            <IonIcon icon={cameraOutline}></IonIcon>
+            <IonIcon class="camera" icon={radioButtonOffOutline}></IonIcon>
           </IonFabButton>
         </IonFab>
         <IonActionSheet
