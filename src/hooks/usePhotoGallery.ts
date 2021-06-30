6a07e07c9a070714f98ead0dc4bc8b25b0e86328
Matheus Albuquerque <land@ythecombinator.space>
2021-06-30 21:03:28 +0200

    fix(src/hooks/usePhotoGallery.ts): handle uncaught exception

--- src/hooks/usePhotoGallery.ts
+++ src/hooks/usePhotoGallery.ts
@@ -1,27 +1,31 @@
 import { useState, useEffect } from "react";
 import { isPlatform } from "@ionic/react";
 
-import { Camera, CameraResultType, CameraSource, Photo } from "@capacitor/camera";
+import {
+  Camera,
+  CameraResultType,
+  CameraSource,
+  Photo,
+} from "@capacitor/camera";
 import { Filesystem, Directory } from "@capacitor/filesystem";
 import { Storage } from "@capacitor/storage";
 import { Capacitor } from "@capacitor/core";
 
 const PHOTO_STORAGE = "photos";
 export function usePhotoGallery() {
-
   const [photos, setPhotos] = useState<UserPhoto[]>([]);
 
   useEffect(() => {
     const loadSaved = async () => {
-      const {value} = await Storage.get({key: PHOTO_STORAGE });
+      const { value } = await Storage.get({ key: PHOTO_STORAGE });
 
       const photosInStorage = (value ? JSON.parse(value) : []) as UserPhoto[];
       // If running on the web...
-      if (!isPlatform('hybrid')) {
+      if (!isPlatform("hybrid")) {
         for (let photo of photosInStorage) {
           const file = await Filesystem.readFile({
             path: photo.filepath,
-            directory: Directory.Data
+            directory: Directory.Data,
           });
           // Web platform only: Load the photo as base64 data
           photo.webviewPath = `data:image/jpeg;base64,${file.data}`;
@@ -33,24 +37,31 @@ export function usePhotoGallery() {
   }, []);
 
   const takePhoto = async () => {
-    const cameraPhoto = await Camera.getPhoto({
-      resultType: CameraResultType.Uri,
-      source: CameraSource.Camera,
-      quality: 100
-    });
-    const fileName = new Date().getTime() + '.jpeg';
-    const savedFileImage = await savePicture(cameraPhoto, fileName);
-    const newPhotos = [savedFileImage, ...photos];
-    setPhotos(newPhotos);
-    Storage.set({key: PHOTO_STORAGE,value: JSON.stringify(newPhotos)});
+    try {
+      const cameraPhoto = await Camera.getPhoto({
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Camera,
+        quality: 100,
+      });
+      const fileName = new Date().getTime() + ".jpeg";
+      const savedFileImage = await savePicture(cameraPhoto, fileName);
+      const newPhotos = [savedFileImage, ...photos];
+      setPhotos(newPhotos);
+      Storage.set({ key: PHOTO_STORAGE, value: JSON.stringify(newPhotos) });
+    } catch (err) {
+      console.log(err);
+    }
   };
 
-  const savePicture = async (photo: Photo, fileName: string): Promise<UserPhoto> => {
+  const savePicture = async (
+    photo: Photo,
+    fileName: string
+  ): Promise<UserPhoto> => {
     let base64Data: string;
     // "hybrid" will detect Cordova or Capacitor;
-    if (isPlatform('hybrid')) {
+    if (isPlatform("hybrid")) {
       const file = await Filesystem.readFile({
-        path: photo.path!
+        path: photo.path!,
       });
       base64Data = file.data;
     } else {
@@ -59,39 +70,38 @@ export function usePhotoGallery() {
     const savedFile = await Filesystem.writeFile({
       path: fileName,
       data: base64Data,
-      directory: Directory.Data
+      directory: Directory.Data,
     });
 
-    if (isPlatform('hybrid')) {
+    if (isPlatform("hybrid")) {
       // Display the new image by rewriting the 'file://' path to HTTP
       // Details: https://ionicframework.com/docs/building/webview#file-protocol
       return {
         filepath: savedFile.uri,
         webviewPath: Capacitor.convertFileSrc(savedFile.uri),
       };
-    }
-    else {
+    } else {
       // Use webPath to display the new image instead of base64 since it's
       // already loaded into memory
       return {
         filepath: fileName,
-        webviewPath: photo.webPath
+        webviewPath: photo.webPath,
       };
     }
   };
 
   const deletePhoto = async (photo: UserPhoto) => {
     // Remove this photo from the Photos reference data array
-    const newPhotos = photos.filter(p => p.filepath !== photo.filepath);
+    const newPhotos = photos.filter((p) => p.filepath !== photo.filepath);
 
     // Update photos array cache by overwriting the existing photo array
-    Storage.set({key: PHOTO_STORAGE, value: JSON.stringify(newPhotos) });
+    Storage.set({ key: PHOTO_STORAGE, value: JSON.stringify(newPhotos) });
 
     // delete photo file from filesystem
-    const filename = photo.filepath.substr(photo.filepath.lastIndexOf('/') + 1);
+    const filename = photo.filepath.substr(photo.filepath.lastIndexOf("/") + 1);
     await Filesystem.deleteFile({
       path: filename,
-      directory: Directory.Data
+      directory: Directory.Data,
     });
     setPhotos(newPhotos);
   };
@@ -99,7 +109,7 @@ export function usePhotoGallery() {
   return {
     deletePhoto,
     photos,
-    takePhoto
+    takePhoto,
   };
 }
 
@@ -115,10 +125,10 @@ export async function base64FromPath(path: string): Promise<string> {
     const reader = new FileReader();
     reader.onerror = reject;
     reader.onload = () => {
-      if (typeof reader.result === 'string') {
+      if (typeof reader.result === "string") {
         resolve(reader.result);
       } else {
-        reject('method did not return a string')
+        reject("method did not return a string");
       }
     };
     reader.readAsDataURL(blob);
