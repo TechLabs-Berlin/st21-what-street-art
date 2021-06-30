47997a4170cb45125ae87a3f1ec81db2cc839308
Matheus Albuquerque <land@ythecombinator.space>
2021-06-30 19:00:06 +0200

    fix: fix compile errors

--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -4,57 +4,58 @@ import "./LeafletMap.css";
 
 import ArtSites from "../data/artworkLocations.json";
 
-import {Icon} from 'leaflet'
-import markerIcon from "../assets/markerIcon.png"
-import shadow from "../assets/shadow.png"
-
-{/* Geolocation */}
+import { Icon } from "leaflet";
+import markerIcon from "../assets/markerIcon.png";
+import shadow from "../assets/shadow.png";
 import useGeolocation from "../hooks/useGeolocation";
 
-{/* Geolocation */}
-const location = useGeolocation();
+{
+  /* Geolocation */
+}
 
-{/* Starting position of Map */}
-const position = { lat: 52.5200, lng: 13.4050 };
+{
+  /* Geolocation */
+}
+
+{
+  /* Starting position of Map */
+}
+const position = { lat: 52.52, lng: 13.405 };
 
 const LeafletMap: FC = () => {
+  const location = useGeolocation();
+  console.log(location);
 
   return (
     <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
-      />
-      
-      
-      {ArtSites.map(artw => (
-      <Marker 
-      key = {artw.id}
-      position = {[artw["location/lat"], artw["location/lng"]]}
-      icon = {new Icon({
-        iconUrl: markerIcon,
-        iconSize: [25, 41],
-        shadowUrl: shadow,
-        shadowSize: [41, 41],
-        iconAnchor: [12, 41],
-	      popupAnchor: [1, -34]
-        })}>
-     
-          <Popup position = {[artw["location/lat"], artw["location/lng"]]}>
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+      {ArtSites.map((artw) => (
+        <Marker
+          key={artw.id}
+          position={[artw["location/lat"], artw["location/lng"]]}
+          icon={
+            new Icon({
+              iconUrl: markerIcon,
+              iconSize: [25, 41],
+              shadowUrl: shadow,
+              shadowSize: [41, 41],
+              iconAnchor: [12, 41],
+              popupAnchor: [1, -34],
+            })
+          }
+        >
+          <Popup position={[artw["location/lat"], artw["location/lng"]]}>
             <div>
-              <h5 className = "artMarkerTitle">
-                {artw.title}
-              </h5>
-              <h6>
-                {artw["location/address"]}
-              </h6>
-              <div className = "thumbnailContainer"> 
-                	<img id="thumbnail" src={artw.thumbnail}></img>
+              <h5 className="artMarkerTitle">{artw.title}</h5>
+              <h6>{artw["location/address"]}</h6>
+              <div className="thumbnailContainer">
+                <img id="thumbnail" src={artw.thumbnail}></img>
               </div>
             </div>
           </Popup>
-      </Marker>
-
-    ))}
-  
+        </Marker>
+      ))}
 
       <div id="leaflet-map"></div>
     </MapContainer>
--- src/hooks/useGeolocation.ts
+++ src/hooks/useGeolocation.ts
@@ -1,39 +1,48 @@
-const useGeolocation =() => {
-    const [] = useState ({
-        loaded: false,
-        coordinates: {lat: "", lng: ""}
-});
+import { useEffect, useState } from "react";
+import { Coordinates } from "../models/location";
 
-    const onSuccess = location => {
-        setLocation({
-            loaded: true,
-            coordinates: {
-                lat: location.coords.latitude,
-                lng: location.coords.longitude,
-            },
-        });
-    };
+interface LocationState {
+  loaded?: boolean;
+  error?: Error;
+  coordinates?: Coordinates;
+}
 
-    const onError = error => {
-        setLocation({
-            loaded: true,
-            error,
-        });
+const useGeolocation = () => {
+  const [location, setLocation] = useState<LocationState>({
+    loaded: false,
+    coordinates: { lat: "", lng: "" },
+    error: null as unknown as Error,
+  });
+
+  const onSuccess = (location: any) => {
+    setLocation({
+      loaded: true,
+      coordinates: {
+        lat: location.coords.latitude,
+        lng: location.coords.longitude,
+      },
+    });
+  };
+
+  const onError = (error: any) => {
+    setLocation({
+      loaded: true,
+      error,
+    });
+  };
+
+  useEffect(() => {
+    if (!("geolocation" in navigator)) {
+      onError({
+        code: 0,
+        message: "Geolocation not supported",
+      });
     }
 
+    navigator.geolocation.getCurrentPosition(onSuccess, onError);
+  }, []);
 
-    useEffect(() => {
-        if ( !("geolocation" in navigator)) {
-           onError({
-                    code: 0,
-                    message: "Geolocation not supported",
-                });
-        }
-
-        navigator.geolocation.getCurrentPosition(onSuccess, onError);
-    }, []);
-
-    return location;
+  return location;
 };
 
 export default useGeolocation;
