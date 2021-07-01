e44c8096ca08d7cc98d51082ed033878176f119e
Matheus Albuquerque <land@ythecombinator.space>
2021-07-01 13:48:25 +0200

    feat(src/api/index.ts): use actual API

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,10 +2,7 @@ import { stringifyUrl } from "query-string";
 import { URL, URLEndpoint } from "../config";
 
 export const getNearYou = async (lat: number, lng: number) => {
-  // TODO: Uncomment once Darine fixes API
-  // const baseUrl = `${URL.base}/${URLEndpoint.nearYou}`;
-  const baseUrl =
-    "https://raw.githubusercontent.com/ythecombinator/wsa-server-mock/main/db.json";
+  const baseUrl = `${URL.base}/${URLEndpoint.nearYou}`;
   const url = stringifyUrl({ url: baseUrl, query: { lat, lng } });
 
   const rawResponse = await fetch(url);
