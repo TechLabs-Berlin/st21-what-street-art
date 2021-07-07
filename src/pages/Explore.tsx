177bb4db488319a629b8c1a31af9678169783e14
loraxxde <llaura.dobson@gmail.com>
2021-07-07 02:29:04 +0200

    imported usePopularArtworks, set limit to 60

--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -1,46 +1,29 @@
 import React, { useState } from "react";
-import { IonContent, IonPage} from '@ionic/react';
+import { IonContent, IonPage } from '@ionic/react';
 import './Explore.css';
 import Header from '../components/Header';
 
 import ImageGrid from '../components/ImageGrid';
 
-import { useArtwork } from "../hooks/artworks";
+import { usePopularArtworks } from "../hooks/artworks";
 
 export const Explore: React.FC = () => {
-    const gridImagesId = useArtwork ({
-      limit: 18,
-      id: string,
-    });
-  
-
-    // Optimizing Page Renders
-    const [isVisible, setIsVisible] = useState(true);
-  
-    useIonViewDidEnter(() => {
-      console.log("useIonViewDidEnter");
-      setIsVisible(true);
-    });
-  
-    useIonViewDidLeave(() => {
-      console.log("useIonViewDidLeave");
-      setIsVisible(false);
-    });
-  
-    if (!isVisible) return null;
-  
+  const popularArtworksData = usePopularArtworks({
+    limit: 60,
+    property: "id",
+  });
 
   return (
     <IonPage>
       <Header />
-          <IonContent>
-            <h1 className="explore">Explore</h1>
+      <IonContent>
+        <h1 className="explore">Explore</h1>
 
-          <ImageGrid data={gridImagesId} />
+        <ImageGrid data={popularArtworksData} />
 
-    </IonContent>
-  </IonPage>
-);
+      </IonContent>
+    </IonPage>
+  );
 };
 
 // The memo uses the memory to render less
