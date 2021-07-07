239a44b3b4821dd11c4747fe2bb764158fad8572
loraxxde <llaura.dobson@gmail.com>
2021-07-07 02:27:16 +0200

    data.map calls an IonCol!

--- src/components/ImageGrid.tsx
+++ src/components/ImageGrid.tsx
@@ -4,39 +4,35 @@ import './ImageGrid.css';
 import { Artwork } from "../models/artwork";
 
 
-{/* Implementing Firebase business */}
+{/* Implementing Firebase business */ }
 interface Props {
-  data?: Artwork[];
+  data: Artwork[];
 }
 
 export const ImageGrid: React.FC<Props> = (props) => {
   const { data } = props;
 
+  return (
+    <IonGrid>
+      <IonRow class="ion-no-margin">
 
-  {data?.map((item) => {
+        {data.map((item) => (
+          <IonCol class="ion-no-padding" size="4">
+            <div className="img-wrap">
+              <a href="/Art">
+                <img className="images" src={item.image} alt="" />
+                <div className="overlay">
+                  <div className="overlayText">{item.title}</div>
+                </div>
+              </a>
+            </div>
+          </IonCol>
+        ))}
 
+      </IonRow>
+    </IonGrid>
+  );
 
-      return (
+}
 
-            <IonGrid>
-              <IonRow class="ion-no-margin">
-
-                <IonCol class="ion-no-padding" size = "4">
-                    <div className="img-wrap">
-                      <a href="/Art"> 
-                        <img className="images" src={item.image} alt=""/>
-                          <div className="overlay">
-                            <div className="overlayText">{item.title}</div>
-                         </div>
-                      </a>
-                    </div>
-                </IonCol>
-
-              </IonRow>
-            </IonGrid>
-
-    )});
-      }};
-
-      
 export default ImageGrid;
